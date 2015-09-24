a1738f6226de6b990217313268199bd62032d84d
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-09-24 21:55:00 +0000

    Production ActionMailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # ActionMailer preferences
+  config.action_mailer.default_url_options = { host: 'letsblocitoff.herokuapp.com/' }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
 end
