ae740625154b84694aca1002af2e978cc40323e3
Anthony Mahadeo <anthonymahadeo@gmail.com>
2015-09-29 22:37:56 +0000

    Added ability to create and view todos

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
          
   validates :username, presence: true        
+  
+  has_many :items, dependent: :destroy
 end
